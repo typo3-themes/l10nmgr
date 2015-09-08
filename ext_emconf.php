c98c37c7e14076719230527e1dc8b9ea088c01f6
Jo Hasenau <jo.hasenau@external.telekom.de>
2015-09-08 15:10:53 +0200

    [BUGFIX] Adjusted ext_emconf.php to avoid upload errors

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,14 +14,24 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Localization Manager',
     'description' => 'Module for managing localization import and export',
     'category' => 'module',
+	'shy' => 0,
     'version' => '5.0.0-dev',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => 'translate,export,import,cm1,cm2,cm3,mod1,mod2',
     'state' => 'beta',
     'uploadfolder' => false,
     'createDirs' => 'uploads/tx_l10nmgr/settings,uploads/tx_l10nmgr/saved_files,uploads/tx_l10nmgr/jobs,uploads/tx_l10nmgr/jobs/out,uploads/tx_l10nmgr/jobs/in,uploads/tx_l10nmgr/jobs/done,uploads/tx_l10nmgr/jobs/_cmd',
+	'modify_tables' => '',
     'clearcacheonload' => true,
+	'lockType' => '',
     'author' => 'Kasper Skaarhoej, Daniel Zielinski, Daniel Poetzinger, Fabian Seltmann, Andreas Otto, Jo Hasenau, Peter Russ',
     'author_email' => 'kasperYYYY@typo3.com, info@loctimize.com, info@cybercraft.de, pruss@uon.li',
     'author_company' => 'Localization Manager Team',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
     'constraints' =>
         array(
             'depends' =>
@@ -34,5 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
             'suggests' =>
                 array(),
         ),
+	'_md5_values_when_last_written' => '',
+	'suggests' => array()
 );
 
