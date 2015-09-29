7faeb8ab9f22dcd1ea2f95ea16e5064ed87452e2
Jo Hasenau <jo.hasenau@external.telekom.de>
2015-09-29 21:34:22 +0200

    [TASK] Added htmlspecialchars to get correct matching

--- Classes/Model/TranslationDataFactory.php
+++ Classes/Model/TranslationDataFactory.php
@@ -25,6 +25,7 @@ namespace Localizationteam\L10nmgr\Model;
  ***************************************************************/
 
 use TYPO3\CMS\Core\Html\RteHtmlParser;
+use TYPO3\CMS\Core\Utility\DebugUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Localizationteam\L10nmgr\Model\TranslationData;
 use Localizationteam\L10nmgr\Model\Tools\XmlTools;
@@ -117,20 +118,22 @@ class TranslationDataFactory
                             }
                             $pattern2 = '/' . preg_replace('/\//i', '\/', preg_quote($pattern)) . '/';
                             $pattern = '/^' . preg_replace('/\//i', '\/', preg_quote($pattern)) . '/';
+	                        $originalValue = htmlspecialchars($row['XMLvalue'], ENT_COMPAT | ENT_IGNORE | ENT_XHTML,
+		                        'UTF-8', false);
                             if (TYPO3_DLOG) {
                                 GeneralUtility::sysLog(__FILE__ . ': ' . __LINE__ . ': Pattern: ' . $pattern,
                                     'l10nmgr');
                                 GeneralUtility::sysLog(__FILE__ . ': ' . __LINE__ . ': Pattern 2: ' . $pattern2,
                                     'l10nmgr');
                             }
-                            if (preg_match($pattern, $row['XMLvalue'], $treffer)) {
+                            if (preg_match($pattern, $originalValue, $treffer)) {
                                 if (TYPO3_DLOG) {
                                     GeneralUtility::sysLog(__FILE__ . ': ' . __LINE__ . ': Start row[values][0] eq start row[XMLvalue]!!!' . LF . 'XMLvalue: ' . $row['XMLvalue'],
                                         'l10nmgr');
                                 }
                                 $translation[$attrs['table']][$attrs['elementUid']][$attrs['key']] = $row['XMLvalue'];
-                            } elseif ((preg_match('/<[^>]+>/i', $row['XMLvalue'])) && (!preg_match($pattern2,
-                                    $row['XMLvalue'], $treffer))
+                            } elseif ((preg_match('/<[^>]+>/i', $originalValue)) && (!preg_match($pattern2,
+		                            $originalValue, $treffer))
                             ) {
                                 if (TYPO3_DLOG) {
                                     GeneralUtility::sysLog(__FILE__ . ': ' . __LINE__ . ': TAG found in: ' . $row['XMLvalue'],
